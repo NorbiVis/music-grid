4bc1b7927becd0bbb400e055aa45685231f41ca6
NorbiVis <norbi_vis@yahoo.com>
2021-05-12 19:03:47 +0300

    exercitiul 1_2

--- tema_curs_1_2.js
+++ tema_curs_1_2.js
@@ -1,5 +1,16 @@
 const URL = ' https://randomuser.me/api/'
 
 async function utilizator() {
-    document.querySelector('util')
+    const rezultat = await fetch(URL);
+    const date = await rezultat.json();
+    const utilizator = date.results[0];
+    console.log(utilizator)
+    document.getElementById('img').setAttribute('src', utilizator.picture.medium)
+    document.getElementById('gender').innerHTML = utilizator.gender;
+    document.getElementById('nume').innerHTML = utilizator.name.first + " " + utilizator.name.last;
+    document.getElementById('age').innerHTML = utilizator.dob.age;
+    document.getElementById('email').innerHTML = utilizator.email;
 }
+
+
+document.getElementById('persoana').addEventListener('click', utilizator);
